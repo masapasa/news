e84477b0a8b1f6b34e17936486c6a1dd8a5faf38
aswin <apyakurel@gmail.com>
2020-11-27 10:52:07 +0100

    msg

--- next/components/common/Footer.js
+++ next/components/common/Footer.js
@@ -64,7 +64,7 @@ export default function Footer() {
           <Stats />
 
           <p>&copy; 2019-2020. info@nubri.co</p>
-          <p>No claims are made on content presented here.</p>
+          <p>No claims are made on content presented here. Only for info</p>
 
           <img className="h-8 my-8 mx-auto" src="https://a0.awsstatic.com/libra-css/images/logos/aws_smile-header-desktop-en-white_59x35@2x.png" alt="AWS News" />
           <div className="h-12 bg-gray-100"></div>
